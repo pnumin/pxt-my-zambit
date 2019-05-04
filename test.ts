c397ce5aa6bdc886cdb2346225fdc74d7ad06502
pnumin <49543573+pnumin@users.noreply.github.com>
2019-05-04 11:07:43 +0900

    add test

--- test.ts
+++ test.ts
@@ -1,1 +1,16 @@
 // tests go here; this will not be compiled when this package is used as a library
+namespace basic {
+
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
