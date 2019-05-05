3cd6c96f04e032008d2714de9435faa207245c13
pnumin <49543573+pnumin@users.noreply.github.com>
2019-05-05 21:21:36 +0900

    code test4

--- zambit.ts
+++ zambit.ts
@@ -1,6 +1,6 @@
 // 여기에 코드를 추가하세요.
 
-//% color=109 weight=50 icon="\uf121" block="zambit"
+//% color=109 weight=120 icon="\uf121" block="zambit"
 namespace zambit {
     //% block
     export function 안녕() {
