3a5fd15f49faf72e780f0e9995953db3d5dd267d
pnumin <49543573+pnumin@users.noreply.github.com>
2019-05-05 21:28:50 +0900

    code test6

--- zambit.ts
+++ zambit.ts
@@ -1,9 +1,17 @@
 // 여기에 코드를 추가하세요.
 
-//% color=109 weight=110 icon="\uf121" block="zambit"
+//% color=#222222 weight=110 icon="\uf121" block="zambit"
 namespace zambit {
     //% block
-    export function helloWorld() {
+    export function 안녕() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function 안녕2() {
 
     }
 }
