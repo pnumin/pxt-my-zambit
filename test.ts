6d427ba85178aa628ccc6e748be4fd41a51cdc06
pnumin <49543573+pnumin@users.noreply.github.com>
2019-05-05 21:20:34 +0900

    code test3

--- test.ts
+++ test.ts
@@ -1,19 +1,1 @@
 // tests go here; this will not be compiled when this package is used as a library
-
-//% color=116 weight=100 icon="\uf121" block="zambit Blocks"
-namespace zambit {
-
-    //% block
-    export function 안녕() {
-
-    }
-
-    // note that Caml casing yields lower case
-    // block text with spaces
-
-
-    //% block
-    export function h1() {
-
-    }
-}
--- zambit.ts
+++ zambit.ts
@@ -1,6 +1,9 @@
 // 여기에 코드를 추가하세요.
 
-//% color=116 weight=100 icon="\uf121" block="zambit Blocks"
+//% color=109 weight=50 icon="\uf121" block="zambit"
 namespace zambit {
+    //% block
+    export function 안녕() {
 
+    }
 }
