95a9bded395b858030e483b87f439edf9d3dc4c1
pnumin <49543573+pnumin@users.noreply.github.com>
2019-05-25 17:28:18 +0900

    업데이트.

--- zambit.ts
+++ zambit.ts
@@ -3,6 +3,71 @@
 //% groups="['others', 'Fruits', 'Veggies']"
 
 namespace zambit {
+    //Registers (command) for MAX7219
+    const _NOOP = 0 // no-op (do nothing, doesn't change current status)
+    const _DIGIT = [1, 2, 3, 4, 5, 6, 7, 8] // digit (LED column)
+    const _DECODEMODE = 9 // decode mode (1=on, 0-off; for 7-segment display on MAX7219, no usage here)
+    const _INTENSITY = 10 // intensity (LED brightness level, 0-15)
+    const _SCANLIMIT = 11 // scan limit (number of scanned digits)
+    const _SHUTDOWN = 12 // turn on (1) or off (0)
+    const _DISPLAYTEST = 15 // force all LEDs light up, no usage here
+
+    let _pinCS = DigitalPin.P16 // LOAD pin, 0=ready to receive command, 1=command take effect
+    let _matrixNum = 1 // number of MAX7219 matrix linked in the chain
+    let _displayArray: number[] = [] // display array to show accross all matrixs
+    let _rotation = 0 // rotate matrixs display for 4-in-1 modules
+    let _reversed = false // reverse matrixs display order for 4-in-1 modules
+
+
+    /**
+    * Setup/reset MAX7219s. If you are using 4-in-1 module you'll need to set rotation as true. If your chain are consisted of single modules set it as false (default).
+    */
+    //% block="Setup MAX7219:|Number of matrixs $num|CS(LOAD) = $cs|MOSI(DIN) = $mosi|MISO(not used) = $miso|SCK(CLK) = $sck"
+    //% num.min=1 num.defl=1 cs.defl=DigitalPin.P16 mosi.defl=DigitalPin.P15 miso.defl=DigitalPin.P14 sck.defl=DigitalPin.P13 rotate.defl=false group="1. Setup"
+    export function setup(num: number, cs: DigitalPin, mosi: DigitalPin, miso: DigitalPin, sck: DigitalPin) {
+        // set internal variables        
+        _pinCS = cs
+        _matrixNum = num
+        // prepare display array (for displaying texts; add extra 8 columns at each side as buffers)
+        for (let i = 0; i < (num + 2) * 8; i++)  _displayArray.push(0)
+        // set micro:bit SPI
+        pins.spiPins(mosi, miso, sck)
+        pins.spiFormat(8, 3)
+        pins.spiFrequency(1000000)
+        // initialize MAX7219s
+        _registerAll(_SHUTDOWN, 0) // turn off
+        _registerAll(_DISPLAYTEST, 0) // test mode off
+        _registerAll(_DECODEMODE, 0) // decode mode off
+        _registerAll(_SCANLIMIT, 7) // set scan limit to 7 (column 0-7)
+        _registerAll(_INTENSITY, 15) // set brightness to 15
+        _registerAll(_SHUTDOWN, 1) // turn on
+        clearAll() // clear screen on all MAX7219s
+    }
+
+    /**
+    * (internal function) write command and data to all MAX7219s
+    */
+    function _registerAll(addressCode: number, data: number) {
+        pins.digitalWritePin(_pinCS, 0) // LOAD=LOW, start to receive commands
+        for (let i = 0; i < _matrixNum; i++) {
+            // when a MAX7219 received a new command/data set
+            // the previous one would be pushed to the next matrix along the chain via DOUT
+            pins.spiWrite(addressCode) // command (8 bits)
+            pins.spiWrite(data) //data (8 bits)
+        }
+        pins.digitalWritePin(_pinCS, 1) // LOAD=HIGH, commands take effect
+    }
+
+    /**
+    * Turn off LEDs on all MAX7219s
+    */
+    //% block="Clear all LEDs" group="3. Basic light control"
+    export function clearAll() {
+        for (let i = 0; i < 8; i++) _registerAll(_DIGIT[i], 0)
+    }
+
+
+
     //% block="zamPin x = $x"
     export function zamLed(x: string) {
 
