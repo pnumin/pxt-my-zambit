53830ab6f21b247e6a94254c328e335e659ddfd1
pnumin <49543573+pnumin@users.noreply.github.com>
2019-06-07 13:48:31 +0900

    업데이트.

--- zambitbasic.ts
+++ zambitbasic.ts
@@ -10,6 +10,8 @@ namespace zambitBasic {
         [0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0],
         [1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0]
     ]
+    let ledsize = 5;
+
 
     //% block="zamText text = $text" group="한글"
     export function zamText(text: string) {
@@ -30,16 +32,18 @@ namespace zambitBasic {
     //% block="zamTextScroll text = $text" group="한글"
     export function zamTextScroll(text: string) {
 
+        /**
         for (let i = 0; i < text.length; i++) {
             let idx = hfont.indexOf(text.substr(i, 1))
             if (idx >= 0) {
                 zamLedScroll(idx);
             }
-            basic.pause(1000)
-            basic.clearScreen()
+           
         }
 
+         */
 
+        zamLedScroll(0)
     }
 
 
@@ -58,24 +62,70 @@ namespace zambitBasic {
     }
 
     //LED scroll
+    //i : → , j : ↓
     function zamLedScroll(x: number) {
-        let ledsize = 5 ;
-        for(let i=1; i < ledsize; i++) {
-            let row = 0;
-            let col = ledsize - 1;
-            for (let j = 0; j < ledsize * i - 1; j++) {
-                if (hfont_matrix[x][i] == 1) led.plot(row, col);
-                row++;
-                if (row % 5 == 0) {
-                    col--;
-                    row = 0;
-                }
-            }
-            basic.pause(1000);
-            basic.clearScreen()
+        let dmatrix = disMatrix(hfont_matrix[x]) ;
+
+        for (let cnt = 1; cnt <= ledsize; cnt++ ) {
+            dispalyLed(disScrollMatrix(dmatrix, cnt)); 
+            basic.pause(1000)  
         }
         
     }
 
+    //display matrix
+    function disMatrix(matrix:number[]) {
+        let dmatrix = [[0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0], 
+                       [0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0]
+                      ] ;
 
+        let i = 0;
+        let j = 0;
+        for (let idx = 0; idx < ledsize * ledsize; idx++) {
+            dmatrix[i][j] = matrix[idx] ;
+            i++;
+            if ( i % 5 == 0 ) {
+                j++;
+                i = 0;
+            }    
+        }
+
+        return dmatrix;
+    }
+
+    //display matrix
+    function disScrollMatrix(matrix: number[][], x:number) {
+        let dmatrix = [[0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+        ];
+
+        let originali = 0; 
+        for (let cnt = 1; cnt >= x; cnt++) {
+            for (let i = ledsize - cnt; i <= ledsize -x ; i-- ) {
+                for (let j = 0; j < ledsize; j++) {
+                    dmatrix[i][j] = matrix[originali][j];
+                }
+                originali++;
+            }
+        } 
+
+        return dmatrix;
+    }
+
+
+
+    //led display 
+    function dispalyLed(matrix: number[][]) {
+        for (let i = 0; i < ledsize; i++) {
+            for (let j = 0; j < ledsize; j++) {
+                if (matrix[i][j] == 1) led.plot(i, j);
+            }
+        }
+    }   
 }
